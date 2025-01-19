73f6d06cdd546c367ed0e5cf73a51950a9459d74
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-01-19 20:32:34 +0100

    test scipy-openblas in linux

--- cibw-scripts/cibw_before_build_linux.sh
+++ cibw-scripts/cibw_before_build_linux.sh
@@ -2,12 +2,16 @@ set -xe
 
 PROJECT_DIR="$1"
 
-if [ -e /etc/alpine-release ]; then
-    # musllinux (Alpine Linux)
-    echo "Detected musllinux environment. Installing OpenBLAS using apk..."
-    apk add --no-cache openblas-dev
-else
-    # manylinux (CentOS)
-    echo "Detected manylinux environment. Installing OpenBLAS using yum..."
-    yum install -y openblas-devel
-fi
+# this works
+# if [ -e /etc/alpine-release ]; then
+#     # musllinux (Alpine Linux)
+#     echo "Detected musllinux environment. Installing OpenBLAS using apk..."
+#     apk add --no-cache openblas-dev
+# else
+#     # manylinux (CentOS)
+#     echo "Detected manylinux environment. Installing OpenBLAS using yum..."
+#     yum install -y openblas-devel
+# fi
+
+pip install scipy-openblas32
+python -c "import scipy_openblas32; print(scipy_openblas32.get_pkg_config())" > $PROJECT_DIR/scipy-openblas.pc
