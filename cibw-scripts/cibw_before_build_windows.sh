6b64318e912e30213537fe913d51571c637347e3
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-01-19 16:40:03 +0100

    try scipy-openblas in windows

--- cibw-scripts/cibw_before_build_windows.sh
+++ cibw-scripts/cibw_before_build_windows.sh
@@ -2,4 +2,9 @@ set -xe
 
 PROJECT_DIR="$1"
 
+# install OpenBLAS
+pip install scipy-openblas32
+python -c "import scipy_openblas32; print(scipy_openblas32.get_pkg_config())" > $PROJECT_DIR/scipy-openblas.pc
+
+# install delvewheel to repare wheels
 pip install delvewheel
