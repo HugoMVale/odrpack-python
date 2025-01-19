9b5aa1d206a701768d7ba07cb6d4798928920691
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-01-19 18:31:33 +0100

    try fix windows build

--- cibw-scripts/cibw_before_build_windows.sh
+++ cibw-scripts/cibw_before_build_windows.sh
@@ -3,6 +3,7 @@ set -xe
 PROJECT_DIR="$1"
 
 # install OpenBLAS
+
 pip install scipy-openblas32
 python -c "import scipy_openblas32; print(scipy_openblas32.get_pkg_config())" > $PROJECT_DIR/scipy-openblas.pc
 
