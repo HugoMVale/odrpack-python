3ba83f8bc7788eb6e32b79c46e36a8f67a1f8920
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-01-20 21:38:15 +0100

    remove gcc update, as it fails

--- cibw-scripts/before_build_linux.sh
+++ cibw-scripts/before_build_linux.sh
@@ -8,10 +8,7 @@ if [ -e /etc/alpine-release ]; then
     apk add --no-cache openblas-dev
 else
     # manylinux (CentOS)
-    echo "Detected manylinux environment. Installing OpenBLAS and GCC-13 using yum..."
-    yum install -y centos-release-scl
-    yum install -y devtoolset-13
+    echo "Detected manylinux environment. Installing OpenBLAS using yum..."
     yum install -y openblas-devel
-    scl enable devtoolset-13 bash
 fi
 
