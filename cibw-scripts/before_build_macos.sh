3220cd55df35e7431b52b8f79582a7c431f24319
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-01-20 21:39:16 +0100

    try install openblas and see Accelerate version

--- cibw-scripts/before_build_macos.sh
+++ cibw-scripts/before_build_macos.sh
@@ -4,3 +4,9 @@ PROJECT_DIR="$1"
 PLATFORM=$(uname -m)
 echo $PLATFORM
 
+brew install openblas
+
+cd /System/Library/Frameworks/Accelerate.framework
+ls -l
+file Versions/Current/Accelerate
+otool -L Versions/Current/Accelerate
