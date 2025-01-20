1b516abc49afa5f08fec0359b9efb2e0ea164d77
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-01-20 23:24:10 +0100

    try fix openblas in windows

--- cibw-scripts/before_all_windows.sh
+++ cibw-scripts/before_all_windows.sh
@@ -6,5 +6,10 @@ PROJECT_DIR="$1"
 curl -L -o OpenBLAS-0.3.29_x64.zip https://github.com/OpenMathLib/OpenBLAS/releases/download/v0.3.29/OpenBLAS-0.3.29_x64.zip
 unzip OpenBLAS-0.3.29_x64.zip -d openblas
 
-# add OpenBLAS to PATH
-echo "CIBW_ENVIRONMENT_WINDOWS=PATH=%PATH%;$PROJECT_DIR/openblas" >> $env:GITHUB_ENV
+# Use PROJECT_DIR to set the paths correctly relative to the project root
+export LIBRARY_PATH="$PROJECT_DIR/openblas/lib"
+export INCLUDE="$PROJECT_DIR/openblas/include"
+export PKG_CONFIG_PATH="$PROJECT_DIR/openblas/lib/pkgconfig"
+
+# Set the PATH to include OpenBLAS bin directory
+echo "CIBW_ENVIRONMENT_WINDOWS=LIBRARY_PATH=$LIBRARY_PATH INCLUDE=$INCLUDE PATH=%PATH%;$PROJECT_DIR/openblas/bin"
