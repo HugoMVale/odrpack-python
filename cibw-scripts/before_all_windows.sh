5e3c01becd06f9cd225f2dc705a4adce9c4557ec
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-01-21 22:32:27 +0100

    try fix openblas in windows

--- cibw-scripts/before_all_windows.sh
+++ cibw-scripts/before_all_windows.sh
@@ -8,13 +8,16 @@ unzip OpenBLAS-0.3.29_x64.zip -d openblas
 pwd
 ls -l
 
-# install pkgconfiglite
+# install pkgconfiglite, because the image constains a broken version
 choco install -y --no-progress --stoponfirstfailure --checksum 6004DF17818F5A6DBF19CB335CC92702 pkgconfiglite
 
-# Use PROJECT_DIR to set the paths correctly relative to the project root
-export LIBRARY_PATH="$PROJECT_DIR/openblas/lib"
-export INCLUDE="$PROJECT_DIR/openblas/include"
-export PKG_CONFIG_PATH="$PROJECT_DIR/openblas/lib/pkgconfig"
+echo $PKG_CONFIG_PATH
+pkg-config --variable pc_path pkg-config
 
-# Set the PATH to include OpenBLAS bin directory
-echo "CIBW_ENVIRONMENT_WINDOWS=LIBRARY_PATH=$LIBRARY_PATH INCLUDE=$INCLUDE PATH=%PATH%;$PROJECT_DIR/openblas/bin"
+# Use PROJECT_DIR to set the paths correctly relative to the project root
+# export LIBRARY_PATH="$PROJECT_DIR/openblas/lib"
+# export INCLUDE="$PROJECT_DIR/openblas/include"
+# export PKG_CONFIG_PATH="$PROJECT_DIR/openblas/lib/pkgconfig"
+
+# # Set the PATH to include OpenBLAS bin directory
+# echo "CIBW_ENVIRONMENT_WINDOWS=LIBRARY_PATH=$LIBRARY_PATH INCLUDE=$INCLUDE PATH=%PATH%;$PROJECT_DIR/openblas/bin"
