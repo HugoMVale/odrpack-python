ade531431b50c501c336d3739a538709b0737ea5
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-01-23 17:48:58 +0100

    install pkg-config and add it to PATH

--- cibw-scripts/before_all_windows.sh
+++ cibw-scripts/before_all_windows.sh
@@ -2,6 +2,12 @@ set -xe
 
 PROJECT_DIR="$1"
 
+# Check if gcc and gfortran are available
+which gcc
+which gfortran
+gcc --version
+gfortran --version
+
 # Temporarily add MSYS2 to PATH
 PATH="/c/msys64/usr/bin:$PATH"
 
@@ -9,11 +15,12 @@ PATH="/c/msys64/usr/bin:$PATH"
 pacman -Sy --noconfirm
 pacman -Suu --noconfirm
 
-# Install OpenBLAS
+# Install OpenBLAS and pkg-config
 pacman -S --noconfirm mingw-w64-x86_64-openblas
+pacman -S --noconfirm mingw-w64-x86_64-pkg-config
 
+# Add only /mingw64/bin to PATH
+export PATH="/mingw64/bin:$PATH"
 
-# Check if gcc and gfortran are available
-which gcc
-which gfortran
-
+# Verify OpenBLAS detection
+pkg-config --modversion openblas
