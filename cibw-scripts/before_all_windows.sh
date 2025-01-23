4e8fa757b71a7b9c5f34685479126c9b9236ed40
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-01-23 23:29:11 +0100

    try fix pkg-config

--- cibw-scripts/before_all_windows.sh
+++ cibw-scripts/before_all_windows.sh
@@ -16,25 +16,28 @@ pacman -S --noconfirm mingw-w64-x86_64-openblas
 # pacman -S --noconfirm mingw-w64-x86_64-pkg-config
 
 # Set PKG_CONFIG_PATH for OpenBLAS
-ls -l /mingw64/lib/pkgconfig
-PKG_CONFIG_LITE="/c/ProgramData/Chocolatey/lib/pkgconfiglite/tools/pkg-config-lite-0.28-1"
-mkdir -p "$PKG_CONFIG_LITE/include"
-mkdir -p "$PKG_CONFIG_LITE/lib"
-mkdir -p "$PKG_CONFIG_LITE/lib/pkgconfig"
+ls -l c/mingw64/lib/pkgconfig
+ls -l c/mingw64/lib/libopenblas.*
+ls -l c/mingw64/bin
+# PKG_CONFIG_LITE="/c/ProgramData/Chocolatey/lib/pkgconfiglite/tools/pkg-config-lite-0.28-1"
+# mkdir -p "$PKG_CONFIG_LITE/include"
+# mkdir -p "$PKG_CONFIG_LITE/lib"
+# mkdir -p "$PKG_CONFIG_LITE/lib/pkgconfig"
 
-cp /mingw64/lib/pkgconfig/openblas.pc "$PKG_CONFIG_LITE/lib/pkgconfig"
-cp -r /mingw64/lib/libopenblas.* "$PKG_CONFIG_LITE/lib"
-cp -r /mingw64/include/* "$PKG_CONFIG_LITE/include"
+# cp /mingw64/lib/pkgconfig/openblas.pc "$PKG_CONFIG_LITE/lib/pkgconfig"
+# cp -r /mingw64/lib/libopenblas.* "$PKG_CONFIG_LITE/lib"
+# cp -r /mingw64/include/* "$PKG_CONFIG_LITE/include"
+# cp -r /mingw64/include/openblas/* "$PKG_CONFIG_LITE/include"
 
-# Debugging: Verify the copied files
-echo "Contents of $PKG_CONFIG_LITE/lib/pkgconfig:"
-ls -l "$PKG_CONFIG_LITE/lib/pkgconfig"
+# # Debugging: Verify the copied files
+# echo "Contents of $PKG_CONFIG_LITE/lib/pkgconfig:"
+# ls -l "$PKG_CONFIG_LITE/lib/pkgconfig"
 
-echo "Contents of $PKG_CONFIG_LITE/include:"
-ls -l "$PKG_CONFIG_LITE/include"
+# echo "Contents of $PKG_CONFIG_LITE/include:"
+# ls -l "$PKG_CONFIG_LITE/include"
 
-echo "Contents of $PKG_CONFIG_LITE/lib:"
-ls -l "$PKG_CONFIG_LITE/lib"
+# echo "Contents of $PKG_CONFIG_LITE/lib:"
+# ls -l "$PKG_CONFIG_LITE/lib"
 
 # Convert MSYS2 path to Unix-style (in case of GitHub Actions path issues)
 # export PKG_CONFIG_PATH=$(cygpath -u "C:/msys64/mingw64/lib/pkgconfig")
