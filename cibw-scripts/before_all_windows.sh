ac8517b97da4a2623df509e485e193ef70c42cc7
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-01-23 17:08:48 +0100

    try understand windows build

--- cibw-scripts/before_all_windows.sh
+++ cibw-scripts/before_all_windows.sh
@@ -2,22 +2,18 @@ set -xe
 
 PROJECT_DIR="$1"
 
-# # install OpenBLAS
-# curl -L -o OpenBLAS-0.3.29_x64.zip https://github.com/OpenMathLib/OpenBLAS/releases/download/v0.3.29/OpenBLAS-0.3.29_x64.zip
-# unzip OpenBLAS-0.3.29_x64.zip -d openblas
-# pwd
-# ls -l
+# Temporarily add MSYS2 to PATH
+PATH="/c/msys64/usr/bin:$PATH"
 
-# install pkgconfiglite, because the image constains a broken version
-choco install -y --no-progress --stoponfirstfailure --checksum 6004DF17818F5A6DBF19CB335CC92702 pkgconfiglite
+# Update pacman database and upgrade system packages
+pacman -Sy --noconfirm
+pacman -Suu --noconfirm
 
-# echo $PKG_CONFIG_PATH
-# pkg-config --variable pc_path pkg-config
+# Install OpenBLAS
+pacman -S --noconfirm mingw-w64-x86_64-openblas
 
-# Use PROJECT_DIR to set the paths correctly relative to the project root
-# export LIBRARY_PATH="$PROJECT_DIR/openblas/lib"
-# export INCLUDE="$PROJECT_DIR/openblas/include"
-# export PKG_CONFIG_PATH="$PROJECT_DIR/openblas/lib/pkgconfig"
 
-# # Set the PATH to include OpenBLAS bin directory
-# echo "CIBW_ENVIRONMENT_WINDOWS=LIBRARY_PATH=$LIBRARY_PATH INCLUDE=$INCLUDE PATH=%PATH%;$PROJECT_DIR/openblas/bin"
+# Check if gcc and gfortran are available
+which gcc
+which gfortran
+
