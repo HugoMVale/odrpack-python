71ea645fe7e48282278f1e06b0df8b892fa99467
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-01-23 18:52:38 +0100

    try fix pkg-config

--- cibw-scripts/before_all_windows.sh
+++ cibw-scripts/before_all_windows.sh
@@ -2,6 +2,10 @@ set -xe
 
 PROJECT_DIR="$1"
 
+# Install pkgconfiglite
+choco install -y --no-progress --stoponfirstfailure pkgconfiglite
+which pkg-config
+
 # Update pacman database and upgrade system packages
 PATH="/c/msys64/usr/bin:$PATH"
 pacman -Sy --noconfirm
@@ -9,7 +13,7 @@ pacman -Suu --noconfirm
 
 # Install OpenBLAS and pkg-config
 pacman -S --noconfirm mingw-w64-x86_64-openblas
-pacman -S --noconfirm mingw-w64-x86_64-pkg-config
+# pacman -S --noconfirm mingw-w64-x86_64-pkg-config
 
 # Set PKG_CONFIG_PATH for OpenBLAS
 ls -l /mingw64/lib/pkgconfig
@@ -21,6 +25,5 @@ export PKG_CONFIG_PATH=$(cygpath -u "C:/msys64/mingw64/lib/pkgconfig")
 echo "PKG_CONFIG_PATH is set to: $PKG_CONFIG_PATH"
 
 # Verify OpenBLAS detection
-which pkg-config
 pkg-config --version
 pkg-config --modversion openblas
