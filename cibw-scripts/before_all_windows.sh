4eab304c7a5bb453803d451044abc1f1a074ab4a
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-01-24 22:13:53 +0100

    add include dir

--- cibw-scripts/before_all_windows.sh
+++ cibw-scripts/before_all_windows.sh
@@ -12,16 +12,16 @@ pacman -Suu --noconfirm
 # Install OpenBLAS
 pacman -S --noconfirm mingw-w64-x86_64-openblas
 
-ls -l
 ls -l /c/msys64/mingw64/lib/pkgconfig/
 ls -l /c/msys64/mingw64/lib/libopenblas.*
 ls -l /c/msys64/mingw64/bin
 
+# Copy OpenBLAS files to mingw64 (where the active compiler toolchain is)
 mkdir -p /c/mingw64/lib/pkgconfig
 cp /c/msys64/mingw64/lib/pkgconfig/openblas.pc /c/mingw64/lib/pkgconfig/
 cp /c/msys64/mingw64/bin/libopenblas.dll /c/mingw64/bin/
 cp /c/msys64/mingw64/lib/libopenblas.*   /c/mingw64/lib/
+cp /c/msys64/mingw64/include/openblas    /c/mingw64/include
 
 # Verify OpenBLAS detection
-pkg-config --variable=pc_path pkg-config
 pkg-config --modversion openblas
