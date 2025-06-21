4e33c6da5353a4f4c6e3abd9c87bf2e5f75a1142
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-06-21 15:58:04 +0200

    regenerate stubs

--- src/odrpack/__odrpack.pyi
+++ src/odrpack/__odrpack.pyi
@@ -4,7 +4,7 @@ from typing import Annotated
 from numpy.typing import ArrayLike
 
 
-def loc_iwork(m: int, npar: int, nq: int) -> dict[str, int]:
+def loc_iwork(m: int, q: int, npar: int) -> dict[str, int]:
     """
     Get storage locations within the integer work space.
 
@@ -12,10 +12,10 @@ def loc_iwork(m: int, npar: int, nq: int) -> dict[str, int]:
     ----------
     m : int
         Number of columns of data in the explanatory variable.
+    q : int
+        Number of responses per observation.
     npar : int
         Number of function parameters.
-    nq : int
-        Number of responses per observation.
 
     Returns
     -------
@@ -23,7 +23,7 @@ def loc_iwork(m: int, npar: int, nq: int) -> dict[str, int]:
         A dictionary containing the 0-based indexes of the integer work array.
     """
 
-def loc_rwork(n: int, m: int, npar: int, nq: int, ldwe: int, ld2we: int, isodr: bool) -> dict[str, int]:
+def loc_rwork(n: int, m: int, q: int, npar: int, ldwe: int, ld2we: int, isodr: bool) -> dict[str, int]:
     """
     Get storage locations within the real work space.
 
@@ -33,10 +33,10 @@ def loc_rwork(n: int, m: int, npar: int, nq: int, ldwe: int, ld2we: int, isodr: 
         Number of observations.
     m : int
         Number of columns of data in the explanatory variable.
+    q : int
+        Number of responses per observation.
     npar : int
         Number of function parameters.
-    nq : int
-        Number of responses per observation.
     ldwe : int
         Leading dimension of the `we` array.
     ld2we : int
@@ -50,7 +50,7 @@ def loc_rwork(n: int, m: int, npar: int, nq: int, ldwe: int, ld2we: int, isodr: 
         A dictionary containing the 0-based indexes of the real work array.
     """
 
-def odr(n: int, m: int, npar: int, nq: int, ldwe: int, ld2we: int, ldwd: int, ld2wd: int, ldifx: int, ldstpd: int, ldscld: int, f: Callable, fjacb: Callable, fjacd: Callable, beta: Annotated[ArrayLike, dict(dtype='float64', order='C')], y: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)], x: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)], delta: Annotated[ArrayLike, dict(dtype='float64', order='C')], we: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)] | None = None, wd: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)] | None = None, ifixb: Annotated[ArrayLike, dict(dtype='int32', order='C', writable=False)] | None = None, ifixx: Annotated[ArrayLike, dict(dtype='int32', order='C', writable=False)] | None = None, stpb: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)] | None = None, stpd: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)] | None = None, sclb: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)] | None = None, scld: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)] | None = None, lower: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)] | None = None, upper: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)] | None = None, work: Annotated[ArrayLike, dict(dtype='float64', order='C')] | None = None, iwork: Annotated[ArrayLike, dict(dtype='int32', order='C')] | None = None, job: int | None = None, ndigit: int | None = None, taufac: float | None = None, sstol: float | None = None, partol: float | None = None, maxit: int | None = None, iprint: int | None = None, errfile: str | None = None, rptfile: str | None = None) -> int:
+def odr(n: int, m: int, q: int, npar: int, ldwe: int, ld2we: int, ldwd: int, ld2wd: int, ldifx: int, ldstpd: int, ldscld: int, f: Callable, fjacb: Callable, fjacd: Callable, beta: Annotated[ArrayLike, dict(dtype='float64', order='C')], y: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)], x: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)], delta: Annotated[ArrayLike, dict(dtype='float64', order='C')], we: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)] | None = None, wd: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)] | None = None, ifixb: Annotated[ArrayLike, dict(dtype='int32', order='C', writable=False)] | None = None, ifixx: Annotated[ArrayLike, dict(dtype='int32', order='C', writable=False)] | None = None, stpb: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)] | None = None, stpd: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)] | None = None, sclb: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)] | None = None, scld: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)] | None = None, lower: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)] | None = None, upper: Annotated[ArrayLike, dict(dtype='float64', order='C', writable=False)] | None = None, work: Annotated[ArrayLike, dict(dtype='float64', order='C')] | None = None, iwork: Annotated[ArrayLike, dict(dtype='int32', order='C')] | None = None, job: int | None = None, ndigit: int | None = None, taufac: float | None = None, sstol: float | None = None, partol: float | None = None, maxit: int | None = None, iprint: int | None = None, errfile: str | None = None, rptfile: str | None = None) -> int:
     """
     C++ wrapper for the Orthogonal Distance Regression (ODR) routine.
 
@@ -60,14 +60,14 @@ def odr(n: int, m: int, npar: int, nq: int, ldwe: int, ld2we: int, ldwd: int, ld
         Number of observations.
     m : int
         Number of columns in the independent variable data.
+    q : int
+        Number of responses per observation.
     npar : int
         Number of function parameters.
-    nq : int
-        Number of responses per observation.
     ldwe : int
         Leading dimension of the `we` array, must be in `{1, n}`.
     ld2we : int
-        Second dimension of the `we` array, must be in `{1, nq}`.
+        Second dimension of the `we` array, must be in `{1, q}`.
     ldwd : int
         Leading dimension of the `wd` array, must be in `{1, n}`.
     ld2wd : int
@@ -89,13 +89,13 @@ def odr(n: int, m: int, npar: int, nq: int, ldwe: int, ld2we: int, ldwd: int, ld
     beta : np.ndarray[float64]
         Array of function parameters with shape `(npar)`.
     y : np.ndarray[float64]
-        Dependent variables with shape `(nq, n)`. Ignored for implicit models.
+        Dependent variables with shape `(q, n)`. Ignored for implicit models.
     x : np.ndarray[float64]
         Explanatory variables with shape `(m, n)`.
     delta : np.ndarray[float64]
         Initial errors in `x` data with shape `(m, n)`.
     we : np.ndarray[float64], optional
-        Weights for `epsilon` with shape `(nq, ld2we, ldwe)`. Default is None.
+        Weights for `epsilon` with shape `(q, ld2we, ldwe)`. Default is None.
     wd : np.ndarray[float64], optional
         Weights for `delta` with shape `(m, ld2wd, ldwd)`. Default is None.
     ifixb : np.ndarray[int32], optional
@@ -148,7 +148,7 @@ def odr(n: int, m: int, npar: int, nq: int, ldwe: int, ld2we: int, ldwd: int, ld
     - Input arrays will automatically be made contiguous and cast to the correct type if necessary.
     """
 
-def workspace_dimensions(n: int, m: int, npar: int, nq: int, isodr: bool) -> tuple:
+def workspace_dimensions(n: int, m: int, q: int, npar: int, isodr: bool) -> tuple:
     """
     Calculate the dimensions of the workspace arrays.
 
@@ -158,10 +158,10 @@ def workspace_dimensions(n: int, m: int, npar: int, nq: int, isodr: bool) -> tup
         Number of observations.
     m : int
         Number of columns of data in the explanatory variable.
+    q : int
+        Number of responses per observation.
     npar : int
         Number of function parameters.
-    nq : int
-        Number of responses per observation.
     isodr : bool
         Variable designating whether the solution is by ODR (`True`) or by OLS (`False`).
 
