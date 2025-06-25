d2a3267f192768c31808f6790e32b578b8c2469d
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-06-25 22:28:28 +0200

    update

--- cibw-scripts/before_all_linux.sh
+++ cibw-scripts/before_all_linux.sh
@@ -9,11 +9,5 @@ if [ -e /etc/alpine-release ]; then
 else
     # manylinux (CentOS)
     echo "Detected manylinux environment. Installing OpenBLAS using yum..."
-    yum install -y centos-release-scl
-    yum install -y devtoolset-12-gcc devtoolset-12-gcc-gfortran devtoolset-12-gcc-c++
-    source /opt/rh/devtoolset-12/enable
-    export CC=gcc
-    export CXX=g++
-    export FC=gfortran
     yum install -y openblas-devel
 fi
