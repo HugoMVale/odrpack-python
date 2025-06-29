51e1788aeefadbe12878f52514ea042dcee2e8a4
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-06-29 22:30:58 +0200

    bump version to 0.3.1

--- src/odrpack/__init__.py
+++ src/odrpack/__init__.py
@@ -8,10 +8,11 @@ model can be either explicit or implicit. Additionally, `odrpack` can be used to
 solve the ordinary least squares problem where all of the errors are attributed
 to the observations of the dependent variable.
 """
-
-__version__ = '0.3.1'
+import importlib.metadata
 
 from odrpack.exceptions import *
 from odrpack.odr_fortran import *
 from odrpack.odr_scipy import *
 from odrpack.result import *
+
+__version__ = importlib.metadata.version(__name__)
