c2cf4b8b854401d4b7a7e1872a7a8b97274a0944
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-06-29 19:13:31 +0200

    try recent gcc

--- cibw-scripts/before_all_linux.sh
+++ cibw-scripts/before_all_linux.sh
@@ -5,7 +5,8 @@ PROJECT_DIR="$1"
 if [ -e /etc/alpine-release ]; then
     # musllinux (Alpine Linux)
     echo "Detected musllinux environment. Installing OpenBLAS using apk..."
-    apk add --no-cache build-base openblas-dev
+    # apk add --no-cache build-base openblas-dev
+    apk add --no-cache openblas-dev gfortran=14.3.0-r5
 else
     # manylinux (CentOS)
     echo "Detected manylinux environment. Installing OpenBLAS using yum..."
